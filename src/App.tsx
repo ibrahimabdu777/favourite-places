ce83a6f99a75e586a80fd3086b4468e8f24864cb
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-13 11:17:28 +0100

    with formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import MainBody from "./components/MainBody";
 import { greet } from "./utils/greet";
-import Footer from "./components/Footer"
-import header from "./components/Header"
+import Footer from "./components/Footer";
+import header from "./components/Header";
 import Header from "./components/Header";
 function App(): JSX.Element {
   return (
-  <>
-    <Header />
-    <MainBody />
-    <Footer />
-  </>
-  )
+    <>
+      <Header />
+      <MainBody />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
