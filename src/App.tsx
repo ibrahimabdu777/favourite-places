5bb579c21e69225159968b744ddf66e38c45b166
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-13 11:24:01 +0100

    trying to pass checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import MainBody from "./components/MainBody";
-import { greet } from "./utils/greet";
 import Footer from "./components/Footer";
-import header from "./components/Header";
 import Header from "./components/Header";
 function App(): JSX.Element {
   return (
